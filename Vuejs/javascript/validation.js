b65d48142474c3a4aaae5aa86e8a1dda1322fc4d
akikusa <akikusa.t-engineers@tsuchiya-gousei.com>
2023-09-28 14:45:40 +0900

    second

--- Vuejs/javascript/validation.js
+++ Vuejs/javascript/validation.js
@@ -1,11 +1,12 @@
 function emailValidation() {
-    const app = new Vue({ // --1
-        el: '#app', // --2
-        data: {
-          errorMessage: 'Eメールが一致しません', // --3
-        }
-      })  };
-  
-  window.onload = function() {
-    emailValidation();
-  };
+	const app = new Vue({ // --1
+	  el: '#app', // --2
+	  data: {
+	    errorMessage: 'Eメールが一致しません', // --3
+	  }
+	})
+}
+
+window.onload = function() {
+  emailValidation();
+};
